6597091afceb9449b8f3630b18e3bdc9d618a048
CJF2000 <codyforren@icloud.com>
2025-07-22 19:23:10 -0600

    Update app.py

--- app.py
+++ app.py
@@ -33,9 +33,9 @@ Welcome to the 7-day **Crypto Price Predictor**.
 
 📈 Powered by AI (LSTM neural networks)  
 🔒 Access is password-protected — DM [@Forest_Wizard](https://t.me/Forecast_Wizard) to unlock.  
-💸 Suggested donation: **$10/month or 50 for lifetime access**
-Cashapp: ForestWizard777
-Venmo: Forest_Wizard
+💸 Suggested donation: **$10/month or 50 for lifetime access**   
+Cashapp: ForestWizard777   
+Venmo: Forest_Wizard   
 Accept Other Forms of Payment Just DM Me For Access
 """)
 
